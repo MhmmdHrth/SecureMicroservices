1aa2a7534701a626f46d6bdf526e0bcd8758b419
harith <harith.jamdil@cloud-connect.asia>
2021-06-24 00:18:46 +0800

    configure identity server

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -1,3 +1,5 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,13 +14,17 @@ namespace IdentityServer
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                    .AddInMemoryClients(new List<Client>())
+                    .AddInMemoryIdentityResources(new List<IdentityResource>())
+                    .AddInMemoryApiResources(new List<ApiResource>())
+                    .AddInMemoryApiScopes(new List<ApiScope>())
+                    .AddTestUsers(new List<TestUser>())
+                    .AddDeveloperSigningCredential();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,6 +33,7 @@ namespace IdentityServer
             }
 
             app.UseRouting();
+            app.UseIdentityServer();
 
             app.UseEndpoints(endpoints =>
             {
