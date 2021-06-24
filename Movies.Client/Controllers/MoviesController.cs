d0eaaceca37345ea548f360018c6ef406e4c8488
Muhammad Harith <harith781@gmail.com>
2021-06-25 01:20:30 +0800

    add logout function

--- Movies.Client/Controllers/MoviesController.cs
+++ Movies.Client/Controllers/MoviesController.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
@@ -30,6 +32,12 @@ namespace Movies.Client.Controllers
             {
                 Debug.WriteLine($"Claim Type: {claim.Type} - Claim value: {claim.Value}");
             }
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
         }
 
         public async Task<IActionResult> Index()
